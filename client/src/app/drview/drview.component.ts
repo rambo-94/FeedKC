fe5340e2df70ac1cf2094d186f530ae29ab00f8b
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-06-21 23:34:24 -0500

    Implemented the onclick event routes to load various components in required div portion of the page

--- client/src/app/drview/drview.component.ts
+++ client/src/app/drview/drview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ParamMap, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-drview',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drview.component.css']
 })
 export class DrviewComponent implements OnInit {
+  selectedItemId: string;
+  private username: string;
+  constructor(public route:Router,public rou:ActivatedRoute) { }
 
-  constructor() { }
+  public getUsername(){
+    return this.username;
+  };
+
+  public setUsername(value:string){
+    this.username =value;
+  };
+
 
   ngOnInit() {
+
+    this.rou.paramMap.subscribe((params) =>{
+      this.username= params.get("username");
+
+    })
+
+    this.route.navigate(['dashboard'],{relativeTo:this.rou});
+
   }
 
+
+
+
+  clicknav(value: string) {
+
+    this.selectedItemId=value;
+    if(value=='donate'){
+      this.route.navigate(['donate'],{relativeTo:this.rou});
+
+    }else if(value=='dashboard'){
+      this.route.navigate(['dashboard'],{relativeTo:this.rou});
+
+
+    }else if(value=='profile'){
+
+      this.route.navigate(['profile'],{relativeTo:this.rou});
+
+    }
+    else{
+
+      this.route.navigate(['/home']);
+    }
+
+
+  }
+
+  logout() {
+    this.route.navigate(['/home']);
+  }
 }
