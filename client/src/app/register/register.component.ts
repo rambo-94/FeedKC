25f97acdd59790936a42734c69429f5829a671a7
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-06-27 14:45:22 -0500

    Implemented onsubmit method to save registration details to DB

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {ApiserviceService} from "../apiservice.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private fb:FormBuilder,public api:ApiserviceService,public router:Router) { }
+  registerForm=this.fb.group({
+    'username': ['', Validators.required],
+    'email': ['', Validators.required],
+    'password': ['', Validators.required],
+    'cpassword': ['', Validators.required],
+    'address': ['', Validators.required],
+    'donor': ['', Validators.required],
+    'requestor': ['', Validators.required]
+
+  });
   ngOnInit() {
   }
 
+  onSubmit(value: any) {
+    console.log(value);
+    this.api.saveRegDetails(value).subscribe(res => {
+      console.log(res);
+      this.router.navigate(['/home']);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
